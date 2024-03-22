267f5add5618ab407854566e2a82306a9ac357b8
Marco Hui <marco01px2018@gmail.com>
2024-03-21 21:19:42 -0700

    testing scrape_wsj on main

--- news-scraper/main.py
+++ news-scraper/main.py
@@ -1,4 +1,8 @@
+import datetime
 from scrapers import scrape_wsj
 
 if __name__ == "__main__":
-    wsj_headlines = scrape_wsj('2024','03','21')
+    start_date = datetime.date(2023, 3, 19)
+    end_date = datetime.date(2023, 3, 21)
+    wsj = scrape_wsj(start_date, end_date)
+    print(wsj.head(10))
