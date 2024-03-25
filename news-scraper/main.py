1d7b31f1d5617f574e947cc1cb3b71527e7f1488
Marco Hui <marco01px2018@gmail.com>
2024-03-24 22:05:22 -0700

    edits

--- news-scraper/main.py
+++ news-scraper/main.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     start_date = datetime.date(2023, 3, 19)
     end_date = datetime.date(2023, 3, 20)
     wsj = scrape_wsj(start_date, end_date)
-    print(len(wsj))  
+    print(len(wsj))
+    print(wsj.head(5))
