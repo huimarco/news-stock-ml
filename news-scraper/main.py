cabb3183e6c954891fb89be406473bf631a802b9
Marco Hui <marco01px2018@gmail.com>
2024-03-24 18:39:40 -0700

    adjusted function to iterate through pages and dates

--- news-scraper/main.py
+++ news-scraper/main.py
@@ -3,6 +3,6 @@ from scrapers import scrape_wsj
 
 if __name__ == "__main__":
     start_date = datetime.date(2023, 3, 19)
-    end_date = datetime.date(2023, 3, 21)
+    end_date = datetime.date(2023, 3, 20)
     wsj = scrape_wsj(start_date, end_date)
-    print(wsj.head(10))
+    print(len(wsj))  
--- news-scraper/scrapers.py
+++ news-scraper/scrapers.py
@@ -4,55 +4,67 @@ import pandas as pd
 import datetime
 
 # Function to scrape all headlines on WSJ on a given day
-# CHANGE TO ACCEPT DATETIME OBJECTS AND ITERATE THROUGH PAGES
-def scrape_wsj_date(date):
+def scrape_wsj_date(driver, date):
     # Extract year, month, day
     year = str(date.year)
     month = str(date.month).zfill(2)  # Zero-padding for single-digit months
     day = str(date.day).zfill(2)
 
-    # Start a WebDriver (you need to have chromedriver installed in your system and its path added to the environment variables)
-    driver = webdriver.Edge()
-
-    # URL of the webpage to scrape
-    url = f'https://www.wsj.com/news/archive/{year}/{month}/{day}'
-
-    # Open the webpage
-    driver.get(url)
-
-    # Parse the HTML content
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    # Close the WebDriver
-    driver.quit()
-  
     # Initialize lists to store data
     topics = []
     headlines = []
     published_times = []
+    article_links = []
 
-    # Find all articles on the page
-    articles = soup.find_all('article')
+    # Create page counter
+    page_num = 1
 
-    # Extract data from each article
-    for article in articles:
-        # Extract topic
-        topic = article.find('div', class_='WSJTheme--articleType--34Gt-vdG').text.strip()
-        topics.append(topic)
+    # Loop through each page
+    while True:
+        # URL of the webpage to scrape
+        url = f'https://www.wsj.com/news/archive/{year}/{month}/{day}?page={page_num}'
+
+        # Open the webpage
+        driver.get(url)
+
+        # Parse the HTML content
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        # Find all articles on the page
+        articles = soup.find_all('article')
+
+        # Exit loop if no more articles on page
+        if not articles: 
+            break
+
+        # Extract data from each article
+        for article in articles:
+            # Extract topic
+            topic = article.find('div', class_='WSJTheme--articleType--34Gt-vdG').text.strip()
+            topics.append(topic)
+            
+            # Extract headline
+            headline = article.find('span', class_='WSJTheme--headlineText--He1ANr9C').text.strip()
+            headlines.append(headline)
+            
+            # Extract published time
+            published_time = article.find('p', class_='WSJTheme--timestamp--22sfkNDv').text.strip()
+            published_times.append(published_time)
+
+            # Extract article link
+            link = article.find("a", href=True)["href"]
+            article_links.append(link)
         
-        # Extract headline
-        headline = article.find('span', class_='WSJTheme--headlineText--He1ANr9C').text.strip()
-        headlines.append(headline)
-        
-        # Extract published time
-        published_time = article.find('p', class_='WSJTheme--timestamp--22sfkNDv').text.strip()
-        published_times.append(published_time)
+        # Increment page
+        page_num += 1
+
 
     # Create a Pandas DataFrame
     data = {
         'Topic': topics,
         'Headline': headlines,
-        'Published Time': published_times
+        'Published Time': published_times,
+        'URL': article_links
     }
 
     df = pd.DataFrame(data)
@@ -60,10 +72,16 @@ def scrape_wsj_date(date):
     return df
 
 def scrape_wsj(start_date, end_date):
+    # Start a WebDriver (you need to have chromedriver installed in your system and its path added to the environment variables)
+    driver = webdriver.Edge()
+
     # Generate a list of DataFrames for each day
-    dfs = [scrape_wsj_date(current_date) for current_date in pd.date_range(start_date, end_date)]
+    dfs = [scrape_wsj_date(driver, current_date) for current_date in pd.date_range(start_date, end_date)]
     
     # Concatenate all DataFrames in the list into a single DataFrame
     output_df = pd.concat(dfs, ignore_index=True)
+
+    # Close the WebDriver
+    driver.quit()
     
     return output_df
