473b7b6bbaa60f59828c09e1298d936fe7822084
Marco Hui <marco01px2018@gmail.com>
2024-03-24 20:01:39 -0700

    moved dataframe conversion once at the end

--- news-scraper/scrapers.py
+++ news-scraper/scrapers.py
@@ -5,16 +5,11 @@ import datetime
 
 # Function to scrape all headlines on WSJ on a given day
 def scrape_wsj_date(driver, date):
-    # Extract year, month, day
-    year = str(date.year)
-    month = str(date.month).zfill(2)  # Zero-padding for single-digit months
-    day = str(date.day).zfill(2)
+    # Base URL
+    base_url = f'https://www.wsj.com/news/archive/{str(date.year)}/{str(date.month).zfill(2)}/{str(date.day).zfill(2)}?page='
 
     # Initialize lists to store data
-    topics = []
-    headlines = []
-    published_times = []
-    article_links = []
+    all_articles = []
 
     # Create page counter
     page_num = 1
@@ -22,7 +17,7 @@ def scrape_wsj_date(driver, date):
     # Loop through each page
     while True:
         # URL of the webpage to scrape
-        url = f'https://www.wsj.com/news/archive/{year}/{month}/{day}?page={page_num}'
+        url = base_url + str(page_num)
 
         # Open the webpage
         driver.get(url)
@@ -36,28 +31,50 @@ def scrape_wsj_date(driver, date):
         # Exit loop if no more articles on page
         if not articles: 
             break
-
-        # Extract data from each article
-        for article in articles:
-            # Extract topic
-            topic = article.find('div', class_='WSJTheme--articleType--34Gt-vdG').text.strip()
-            topics.append(topic)
-            
-            # Extract headline
-            headline = article.find('span', class_='WSJTheme--headlineText--He1ANr9C').text.strip()
-            headlines.append(headline)
-            
-            # Extract published time
-            published_time = article.find('p', class_='WSJTheme--timestamp--22sfkNDv').text.strip()
-            published_times.append(published_time)
-
-            # Extract article link
-            link = article.find("a", href=True)["href"]
-            article_links.append(link)
+        
+        all_articles.append(articles)
         
         # Increment page
         page_num += 1
 
+    return all_articles
+
+def scrape_wsj(start_date, end_date):
+    # Start a WebDriver (you need to have chromedriver installed in your system and its path added to the environment variables)
+    driver = webdriver.Edge()
+
+    # Generate a list of DataFrames for each day
+    all_articles = [scrape_wsj_date(driver, current_date) for current_date in pd.date_range(start_date, end_date)]
+
+    # Close the WebDriver
+    driver.quit()
+    
+    return all_articles
+
+def extract_wsj_date(articles):
+    # Initialize lists to store data
+    topics = []
+    headlines = []
+    published_times = []
+    article_links = []
+
+    # Extract data from each article
+    for article in articles:
+        # Extract topic
+        topic = article.find('div', class_='WSJTheme--articleType--34Gt-vdG').text.strip()
+        topics.append(topic)
+        
+        # Extract headline
+        headline = article.find('span', class_='WSJTheme--headlineText--He1ANr9C').text.strip()
+        headlines.append(headline)
+        
+        # Extract published time
+        published_time = article.find('p', class_='WSJTheme--timestamp--22sfkNDv').text.strip()
+        published_times.append(published_time)
+
+        # Extract article link
+        link = article.find("a", href=True)["href"]
+        article_links.append(link)
 
     # Create a Pandas DataFrame
     data = {
@@ -66,22 +83,6 @@ def scrape_wsj_date(driver, date):
         'Published Time': published_times,
         'URL': article_links
     }
-
     df = pd.DataFrame(data)
 
     return df
-
-def scrape_wsj(start_date, end_date):
-    # Start a WebDriver (you need to have chromedriver installed in your system and its path added to the environment variables)
-    driver = webdriver.Edge()
-
-    # Generate a list of DataFrames for each day
-    dfs = [scrape_wsj_date(driver, current_date) for current_date in pd.date_range(start_date, end_date)]
-    
-    # Concatenate all DataFrames in the list into a single DataFrame
-    output_df = pd.concat(dfs, ignore_index=True)
-
-    # Close the WebDriver
-    driver.quit()
-    
-    return output_df
